b90591d4cfad969f64ac38d43257d4c4b1a87910
agustinacaceresmeli <agustina.acaceres@mercadolibre.com>
2021-06-28 22:32:58 -0300

    acelerometro

--- LaboratorioProyectoFinal.X/main.c
+++ LaboratorioProyectoFinal.X/main.c
@@ -6,8 +6,11 @@
 #include "mcc_generated_files/system.h"
 #include "mcc_generated_files/pin_manager.h"
 #include "utils/LedsController.h"
+#include "framework/Accelerometer/Accelerometer.h"
+
 
 void blinkLED( void *p_param );
+void getAccelerometerValues(void *p_param);
 
 enum system_state {
     normal,
@@ -17,19 +20,48 @@ enum system_state {
 };
 
 static enum system_state current_state;
+static Accel_t accel;
 
 int main(void)
 {   
-    current_state = danger;
+    current_state = normal;
     SYSTEM_Initialize( );
     
     while(!ACCEL_init()){}
     
     xTaskCreate( blinkLED, "blink leds", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL );
+    xTaskCreate( getAccelerometerValues, " acceletometer", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL );
     vTaskStartScheduler( );
 
     for(;;);
 }
+
+void getAccelerometerValues(void *p_param){
+    
+    while(1){
+        ACCEL_GetAccel (&accel);
+        
+        if(accel.Accel_Y < 0.8 || accel.Accel_Y > -0.8){
+            if((accel.Accel_Z > 0.8 || accel.Accel_Z < -0.8) && (accel.Accel_X > 0.8 || accel.Accel_X < -0.8)){
+                current_state = danger;
+            }
+            else if((accel.Accel_Z > 0.5 || accel.Accel_Z < -0.5) && (accel.Accel_X > 0.8 || accel.Accel_X < -0.8)){
+                current_state = warning;
+            }else{
+                current_state = normal;
+            }
+        }else{
+            current_state = danger;
+        }
+        //else if(accel.Accel_Z > 0.5 || accel.Accel_Z < -0.5){
+        //    current_state = warning;
+        //}
+        //else{
+        //    current_state = normal;
+        //}
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+} 
 
 void blinkLED( void *p_param ){
     bool blinkWarning = true;
@@ -42,17 +74,20 @@ void blinkLED( void *p_param ){
             blinkDanger = true;
         } else if(current_state == warning){
             if(blinkWarning){
-                blinkLed(YELLOW);
+                //blinkLed(YELLOW);
+                setLedColor(YELLOW);
                 blinkWarning = false;
             }
             blinkDanger = true;
         } else if(current_state == danger){
             if(blinkDanger){
-                blinkLed(RED);
+                //blinkLed(RED);
+                setLedColor(RED);
                 blinkDanger = false;
             }
             blinkWarning = true;
         }
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
 
